af891ee61c1e0ded3911eda1a02fd83c8b3248ee
nicolasD <nicolas.david@eleves.ec-nantes.fr>
2013-10-13 21:31:43 +0200

    classes are finished

--- src/com/example/sqlitetest/CommentsDataSource.java
+++ src/com/example/sqlitetest/CommentsDataSource.java
@@ -1,6 +1,11 @@
 package com.example.sqlitetest;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 
@@ -29,6 +34,49 @@ public class CommentsDataSource {
 	}
 	
 	//updating content
-	// need to complete it
+	public Comment createComment (String comment){
+		ContentValues values = new ContentValues(); // se comporte comme une hashmap
+		values.put(MySQLiteHelper.COLUMN_COMMENT, comment);
+		long insertId = database.insert(MySQLiteHelper.TABLE_COMMENTS, null, values);
+		Cursor cursor = 
+				database.query(
+						MySQLiteHelper.TABLE_COMMENTS,
+						allColumns,
+						MySQLiteHelper.COLUMN_ID+" = "+insertId,
+						null, null, null, null);
+		cursor.moveToFirst();
+		Comment newComment = cursorToComment(cursor);//method at the end of the class
+		cursor.close();
+		return newComment;
+	}
 	
+	public void deleteComment(Comment comment){
+		long id = comment.getId();
+		System.out.println("Comment deleted with id: "+ id);
+		database.delete(MySQLiteHelper.TABLE_COMMENTS, MySQLiteHelper.COLUMN_ID+" = "+ id, null);
+	}
+	
+	public List<Comment> getAllComments(){
+		List<Comment> comments = new ArrayList<Comment>();
+		
+		Cursor cursor = database.query(MySQLiteHelper.TABLE_COMMENTS, allColumns, null, null, null, null, null);
+		
+		cursor.moveToFirst();
+		while(!cursor.isAfterLast()){
+			Comment comment = cursorToComment(cursor);
+			comments.add(comment);
+			cursor.moveToNext();
+		}
+		cursor.close();
+		return comments;
+	}
+
+
+  private Comment cursorToComment(Cursor cursor) {
+    Comment comment = new Comment();
+    comment.setId(cursor.getLong(0));
+    comment.setComment(cursor.getString(1));
+    return comment;
+	
+  }
 }
