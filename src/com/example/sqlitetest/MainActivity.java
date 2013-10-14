9ac74081db4dc8f6b138848140cf6eb9700e353b
nicolasD <nicolas.david@eleves.ec-nantes.fr>
2013-10-14 11:02:43 +0200

    creation of a BackTask class which realize a loop *need to create a thread with a timer to improve it* + creation of a Toast and a progressBar to show backTask progress

--- src/com/example/sqlitetest/MainActivity.java
+++ src/com/example/sqlitetest/MainActivity.java
@@ -7,7 +7,6 @@ package com.example.sqlitetest;
 import java.util.List;
 import java.util.Random;
 
-import com.example.sqlitetest.R;
 import android.app.ListActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -15,80 +14,18 @@ import android.view.View.OnClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 
-import java.util.List;
-import java.util.Random;
-
-import android.app.ListActivity;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.ArrayAdapter;
-/*
-public class MainActivity extends ListActivity {
-  private CommentsDataSource datasource;
-
-  @Override
-  public void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
-
-    datasource = new CommentsDataSource(this);
-    datasource.open();
-
-    List<Comment> values = datasource.getAllComments();
-
-    // use the SimpleCursorAdapter to show the
-    // elements in a ListView
-    ArrayAdapter<Comment> adapter = new ArrayAdapter<Comment>(this,
-        android.R.layout.simple_list_item_1, values);
-    setListAdapter(adapter);
-  }
-
-  // Will be called via the onClick attribute
-  // of the buttons in main.xml
-  public void onClick(View view) {
-    @SuppressWarnings("unchecked")
-    ArrayAdapter<Comment> adapter = (ArrayAdapter<Comment>) getListAdapter();
-    Comment comment = null;
-    switch (view.getId()) {
-    case R.id.add:
-      String[] comments = new String[] { "Cool", "Very nice", "Hate it" };
-      int nextInt = new Random().nextInt(3);
-      // save the new comment to the database
-      comment = datasource.createComment(comments[nextInt]);
-      adapter.add(comment);
-      break;
-    case R.id.delete:
-      if (getListAdapter().getCount() > 0) {
-        comment = (Comment) getListAdapter().getItem(0);
-        datasource.deleteComment(comment);
-        adapter.remove(comment);
-      }
-      break;
-    }
-    adapter.notifyDataSetChanged();
-  }
-
-  @Override
-  protected void onResume() {
-    datasource.open();
-    super.onResume();
-  }
-
-  @Override
-  protected void onPause() {
-    datasource.close();
-    super.onPause();
-  }
-
-} 
-
-*/
 public class MainActivity extends ListActivity {
 	
 	private CommentsDataSource datasource;
 	Button add = null;
 	Button delete = null;
+	
+	
+	//creating the widget for AsyncTask
+	//private ProgressBar mProgressBar;
+	private Button mButton;
 	
 
     @Override
@@ -98,9 +35,15 @@ public class MainActivity extends ListActivity {
         layout = (LinearLayout) LinearLayout.inflate(this, R.layout.activity_main, null);
         setContentView(layout);
         
+        //Link to id the Widget for updating database in local
         add = (Button)findViewById(R.id.add);
         delete = (Button)findViewById(R.id.delete);
         
+        //Link to id the Widget for the AsyncTask
+	   //mProgressBar = (ProgressBar) findViewById(R.id.pBAsync);
+	    mButton = (Button) findViewById(R.id.btnLaunch);
+        
+	    
         datasource = new CommentsDataSource(this);
         datasource.open();
         
@@ -113,6 +56,8 @@ public class MainActivity extends ListActivity {
         
         add.setOnClickListener(clickListenerBoutonsAdd);
         delete.setOnClickListener( clickListenerBoutonsDelete);
+        mButton.setOnClickListener( clickListenerBackTask);
+        
         
         
     }
@@ -156,6 +101,14 @@ public class MainActivity extends ListActivity {
     			adapter.notifyDataSetChanged();
     	};
     };
+    
+    private OnClickListener clickListenerBackTask = new OnClickListener(){
+    	public void onClick(View view){
+				BackTask calcul = new BackTask(MainActivity.this);
+				calcul.execute();
+    	};
+    };
+    
     
     
 }
